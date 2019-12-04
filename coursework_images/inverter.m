bc82b759c0f192a6cbb05e3b48b3bdb13955cd7b
Candanedo <u1558174@live.warwick.ac.uk>
2019-12-04 11:26:59 +0000

    merger

---
+++ coursework_images/inverter.m
@@ -0,0 +1,40 @@
+fileId = fopen("H:/Documents/es3f1/es3f1_camera/coursework_images/out.txt", 'r');
+
+formatSpec = '%d'; % '%d %d %d';
+sizeA = [1 Inf];
+A = fscanf(fileId, formatSpec, sizeA);
+
+% group_of_people 1570x2671x3
+% face 409x615x3
+width = 615; 
+height = 409;
+B = uint8(reshape(A, [width, height, 3]));
+
+%imshow(B);
+
+C = B(:,:,1); % red
+D = B(:,:,2); % green
+E = B(:,:,3); % blue
+
+for i = 1:numel(C)
+        C(i) = 255 - C(i);
+        D(i) = 255 - D(i);
+        E(i) = 255 - E(i);
+ end
+
+
+CC = uint8(reshape(C, height, width));
+DD = uint8(reshape(D, height, width));
+EE = uint8(reshape(E, height, width));
+
+F = uint8(zeros(height, width, 3));
+F(:,:,1) = CC;
+F(:,:,2) = DD;
+F(:,:,3) = EE;
+
+
+imshow(F);
+
+
+
+fclose(fileId);
